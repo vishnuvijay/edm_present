56e1499015f03123dbbe798cf1ced10f8bc868aa
vishnuvijay <vishnupdp@gmail.com>
2017-11-26 04:06:58 -0700

    Update views.py

--- ics_tool/views.py
+++ ics_tool/views.py
@@ -19,36 +19,6 @@ from .models import *
 def health(request):
     return HttpResponse("3")
 
-def user_login(request):
-    template_name = 'ics_tool/login.html'
-
-    if request.method == "GET":
-        return render(request, template_name)
-
-    form = LoginForm(request.POST)
-    if form.is_valid():
-      username       = request.POST.get('username', '')
-      password       = request.POST.get('password', '')
-
-      user = authenticate(username=username, password=password)
-
-      if user:
-            if user.is_active:
-                login(request, user)
-                return render(request,'ics_tool/home.html',{})
-            else:
-                # An inactive account was used - no logging in!
-                return HttpResponse("Your account is disabled.")
-      else:
-            # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details"
-            return HttpResponse("Invalid login details supplied.")
-
-
-    print(form.errors)
-
-    return render(request,'ics_tool/login.html',{})
-
 def index(request):
     template_name = 'ics_tool/home.html'
 
