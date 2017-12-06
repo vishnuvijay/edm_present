99baeaf534ea11f1954cdc8ba9cd8dbb9f24e03c
vishnuvijay <vishnupdp@gmail.com>
2017-12-05 22:57:55 -0700

    Update models.py

--- ics_tool/models.py
+++ ics_tool/models.py
@@ -8,7 +8,7 @@ from django.db import models
 class Customer(models.Model):
     CustomerId          = models.IntegerField(unique=True)
     CustomerName        = models.CharField(max_length=200)
-    
+
 class Feedback(models.Model):
     Feedback = models.CharField(max_length=255)
 
@@ -30,91 +30,47 @@ class SearchDonor(models.Model):
 
     SearchQuery        = models.CharField(max_length=255)
 
-class DonationsLog(models.Model):
-
-    logID           = models.IntegerField(primary_key=True)
-    lastModifidDate = models.DateField()
-    lastChangedBy   = models.CharField(max_length=100)
-
-
 class Donations(models.Model):
-    donationID =models.IntegerField(primary_key=True)
-    donation_date=models.DateField()
-    additional_comments=models.CharField(max_length=100)
-    dontype=models.CharField(max_length=20)
+    donationID    = models.IntegerField(primary_key=True)
+    donor_id      = models.ForeignKey(Donors)
+    donation_date = models.DateField()
+    comments      = models.CharField(max_length=100)
 
-class EBDGFOOD(models.Model):
-    EDBGEvent_foodID= models.IntegerField(primary_key=True)
-    ServingsPerGallon=models.IntegerField()
-    EBDGtype=models.CharField(max_length=20)
-    RestaurantName=models.CharField(max_length=100)
+class InKind(models.Model):
+    donationID   = models.ForeignKey(Donations)
+    description  = models.CharField(max_length=1000)
+    isack_sent   = models.BooleanField(default=False)
+    approxValue  = models.IntegerField()
 
-class EMPTYBOWLFOOD(models.Model):
-    EmptyBowlEvent_foodID=models.IntegerField(primary_key=True)
-    food_description=models.CharField(max_length=100)
-    quantity=models.IntegerField()
-    totalServings=models.IntegerField()
-
-class FOOD(models.Model):
-    foodEntryID=models.IntegerField(primary_key=True)
-    foodtype=models.CharField(max_length=100)
-    avgCost=models.IntegerField()
-    totalPounds=models.IntegerField()
-    totalValue=models.IntegerField()
-    NoPounds=models.IntegerField()
-    ServingsperPound=models.IntegerField()
-
-class FOODCATEGORY(models.Model):
-    donationID=models.ForeignKey(Donations, on_delete=models.CASCADE)
-    categoryID=models.IntegerField()
-
-class FOODENTRY(models.Model):
-    CategoryID=models.ForeignKey(FOODCATEGORY, on_delete=models.CASCADE)
-    foodEntryID=models.IntegerField(primary_key=True)
-
-class FOODCATEGORYDESC(models.Model):
-    categoryID=models.ForeignKey(FOODCATEGORY, on_delete=models.CASCADE)
-    description=models.CharField(max_length=100)
-    categoryName=models.CharField(max_length=100)
-
-class FUNDRAISINGEVENTS(models.Model):
-    eventID=models.IntegerField(primary_key=True)
-    estimatedValue=models.IntegerField()
-    receivedDate=models.DateField()
-    location=models.CharField(max_length=100)
+class CatDonations(models.Model):
+    donationID   = models.ForeignKey(Donations)
+    Grocery      = models.IntegerField()
+    Meat         = models.IntegerField()
+    Bread        = models.IntegerField()
+    Produce      = models.IntegerField()
+    Toiletries   = models.IntegerField()
+    Diaper       = models.IntegerField()
+    Other        = models.IntegerField()
+    TotalPounds  = models.IntegerField()
+    desc         = models.CharField(max_length=1000)
 
 class GOLF(models.Model):
-    eventID=models.ForeignKey(FUNDRAISINGEVENTS, on_delete=models.CASCADE)
-    dtype=models.CharField(max_length=100)
+    donationID   = models.ForeignKey(Donations)
+    Donation     = models.CharField(max_length=1000)
+    Value        = models.IntegerField()
+    Contact      = models.CharField(max_length=100)
+    ReceivedBy   = models.CharField(max_length=100)
 
-class ITEMS(models.Model):
-    donationID=models.ForeignKey(Donations, on_delete=models.CASCADE)
-    description=models.CharField(max_length=100)
-    isack_sent=models.BooleanField(default=False)
-    approxValue=models.IntegerField()
-
-class MONETARY (models.Model):
-    donationID=models.ForeignKey(Donations, on_delete=models.CASCADE)
-    amount=models.IntegerField()
-    modeOfPayment=models.CharField(max_length=100)
-
-class Reports(models.Model):
-    reportID=models.IntegerField(primary_key=True)
-    filename=models.CharField(max_length=100)
-    reportgen_date=models.DateField()
-
-class ServiceEvents(models.Model):
-    service_detailID=models.IntegerField(primary_key=True)
-    date_of_service=models.DateField()
-    serviceID=models.IntegerField()
-
-class EBDGRAFFLE(models.Model):
-    eventID=models.ForeignKey(FUNDRAISINGEVENTS, on_delete=models.CASCADE)
-    donation=models.CharField(max_length=100)
-
-class EMPTYBOWLRAFFLEAUCTION(models.Model):
-    eventID=models.ForeignKey(FUNDRAISINGEVENTS, on_delete=models.CASCADE)
-    description=models.CharField(max_length=100)
-    item=models.CharField(max_length=100)
-    status=models.CharField(max_length=100)
-    receivedDate=models.DateField()
+class GenericDonation(models.Model):
+    EventName    = models.CharField(max_length=100)
+    OrgName      = models.CharField(max_length=100)
+    Contact      = models.CharField(max_length=100)
+    FoodDesc     = models.CharField(max_length=100)
+    Gallons      = models.CharField(max_length=100)
+    Servings     = models.IntegerField
+    avgCost      = models.IntegerField()
+    totalCost    = models.IntegerField()
+    GenDonation  = models.CharField(max_length=1000)
+    Value        = models.IntegerField()
+    ReceivedDate = models.DateField()
+    Comments     = models.CharField(max_length=1000)
